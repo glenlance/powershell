8d6867a7925603a773f627c8f62ec36fafd4558c
Glen Lance <collsharect@gmail.com>
2019-04-03 21:09:28 +0800

    first commit for ps_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+function Get-CodeDir{
+	Set-location -Path C:\Users\y5039\code\frontend
+}
+Set-Alias -Name codedir -Value Get-CodeDir 
+
+function Get-GithubDir{
+	Set-location -Path C:\Users\y5039\go\src\github.com
+}
+Set-Alias -Name github -Value Get-GithubDir 
+
+function Get-GolangDir{
+	Set-location -Path C:\Users\y5039\go\src\golang.org
+}
+Set-Alias -Name golang -Value Get-GolangDir 
+
+function Get-GopkgDir{
+	Set-location -Path C:\Users\y5039\go\src\gopkg.in
+}
+Set-Alias -Name gopkg -Value Get-GopkgDir 
+
+function Get-GorootDir{
+	Set-location -Path C:\Users\y5039\go
+}
+Set-Alias -Name gopath -Value Get-GorootDir
